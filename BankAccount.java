e3d14051cab8738b303b81a5bb7095b6b968c005
Balaji-127 <55305631+Balaji-127@users.noreply.github.com>
2019-10-19 11:43:29 +0530

    PIN Change

--- BankAccount.java
+++ BankAccount.java
@@ -34,21 +34,53 @@ class BankAccount
         upin = sc.next();
         if(uid.equals(id) && upin.equals(pin))
         {
-            ATM atm = new ATM();
-            System.out.println("1. Deposit");
-            System.out.println("2. Withdraw");
+            System.out.println("Login Successfull");
+            System.out.println("1. ATM");
+            System.out.println("2. Show Account Details");
+            System.out.println("3. Update PIN");
             int choice = sc.nextInt();
             switch(choice)
             {
                 case 1:
-                    atm.deposit();
+                    ATM atm = new ATM();
+                    System.out.println("1. Deposit");
+                    System.out.println("2. Withdraw");
+                    System.out.print("Enter your choice : ");
+                    int ch = sc.nextInt();
+                    switch(ch)
+                    {
+                        case 1:
+                            atm.deposit();
+                            break;
+                        case 2:
+                            atm.withdraw();
+                            break;
+                        default:
+                            System.out.println("Enter valid choice");
+                    }
                     break;
                 case 2:
-                    atm.withdraw();
+                    System.out.println("Name : "+name);
+                    System.out.println("Account Number : "+accountNumber);
+                    System.out.println("ID : "+id);
+                    System.out.println("Balance : "+balance);
+                    System.out.println("");
                     break;
-                default:
-                    System.out.println("Enter valid choice");
+                case 3:
+                    String oldPass,newPass;
+                    System.out.println("Enter your old PIN : ");
+                    oldPass = sc.next();
+                    if(oldPass.equals(pin))
+                    {
+                        System.out.println("Enter your new PIN : ");
+                        newPass = sc.next();
+                        pin = newPass;
+                    }
+                    else
+                        System.out.println("Old Password is not correct");
+
             }
+            
         }
         else
         {
