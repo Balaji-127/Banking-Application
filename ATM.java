e1a5ebe9278348eb1adf5b59a873beb79ef072ca
Balaji-127 <55305631+Balaji-127@users.noreply.github.com>
2019-10-19 11:05:10 +0530

    Update

--- ATM.java
+++ ATM.java
@@ -7,14 +7,31 @@ class ATM extends BankAccount
     {
         System.out.print("Enter the amount : ");
         int amount = sc.nextInt();
-        balance = BankAccount.balance + amount;
-        System.out.println("The current balance is : "+balance);
+        if(amount > 20000)
+            System.out.println("Cannot deposit more than 20000");
+        else
+        {
+            balance = BankAccount.balance + amount;
+            System.out.println("The current balance is : "+balance);
+        }
     }
     void withdraw()
     {
         System.out.print("Enter the amount : ");
         int amount = sc.nextInt();
-        balance = BankAccount.balance - amount;
-        System.out.println("The current balance is : "+balance);
+        if(amount > 10000)
+            System.out.println("Maximum Withdraw is 10000");
+        else if(amount <= 0)
+            System.out.println("Enter valid value");
+        else if(BankAccount.balance - amount < 5000)
+        {
+            System.out.println("Minimum account balance must be 5000");
+            System.out.println("Maximum withdraw allowed is " + (BankAccount.balance - 5000));
+        }
+        else
+        {
+            balance = BankAccount.balance - amount;
+            System.out.println("The current balance is : "+balance);
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,3 @@
-import java.util.Random;
-import java.util.Scanner;
-
 class Main
 {
     public static void main(String[] args) 
@@ -11,62 +8,6 @@ class Main
         while(flag)
         {
             customer.login();   
-        }
-    }
-}
-
-class BankAccount
-{
-    Scanner sc = new Scanner(System.in);
-    Random rand = new Random();
-    protected static int balance;
-    private int accountNumber;
-    private String pin, id;
-    private String name; 
-    void createAccount()
-    {
-        System.out.print("Enter name : ");
-        name = sc.next();
-        System.out.print("Enter ID : ");
-        id = sc.next();
-        System.out.print("Enter PIN : ");
-        pin = sc.next();
-        System.out.println("Generating accountNumber");
-        accountNumber = rand.nextInt(1000);
-        System.out.println("Your Account Number is "+ accountNumber);
-        System.out.print("Enter initial balance : ");
-        balance = sc.nextInt();
-        System.out.println("Your Initial balance is : "+balance);
-    }
-    void login()
-    {
-        String uid,upin;
-        System.out.println("Enter Login Credentials");
-        System.out.print("Enter ID : ");
-        uid = sc.next();
-        System.out.print("Enter PIN : ");
-        upin = sc.next();
-        if(uid.equals(id) && upin.equals(pin))
-        {
-            ATM atm = new ATM();
-            System.out.println("1. Deposit");
-            System.out.println("2. Withdraw");
-            int choice = sc.nextInt();
-            switch(choice)
-            {
-                case 1:
-                    atm.deposit();
-                    break;
-                case 2:
-                    atm.withdraw();
-                    break;
-                default:
-                    System.out.println("Enter valid choice");
-            }
-        }
-        else
-        {
-            System.out.println("Login Failed");
         }
     }
 }
