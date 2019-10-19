da75caea53c3aa6118c4e0dfc40c8e5c2f6e98c0
Balaji-127 <55305631+Balaji-127@users.noreply.github.com>
2019-10-19 10:47:15 +0530

    'update'

--- Main.java
+++ Main.java
@@ -5,8 +5,13 @@ class Main
 {
     public static void main(String[] args) 
     {
-        BankAccount customer = new BankAccount();customer.createAccount(); 
-        customer.login();   
+        BankAccount customer = new BankAccount();
+        boolean flag = true;
+        customer.createAccount(); 
+        while(flag)
+        {
+            customer.login();   
+        }
     }
 }
 
@@ -14,23 +19,24 @@ class BankAccount
 {
     Scanner sc = new Scanner(System.in);
     Random rand = new Random();
-    int balance;
-    int accountNumber;
-    String pin, id;
-    String name; 
+    protected static int balance;
+    private int accountNumber;
+    private String pin, id;
+    private String name; 
     void createAccount()
     {
         System.out.print("Enter name : ");
-        name = sc.nextLine();
+        name = sc.next();
         System.out.print("Enter ID : ");
-        id = sc.nextLine();
+        id = sc.next();
         System.out.print("Enter PIN : ");
-        pin = sc.nextLine();
+        pin = sc.next();
         System.out.println("Generating accountNumber");
-        accountNumber = rand.nextInt(500);
+        accountNumber = rand.nextInt(1000);
         System.out.println("Your Account Number is "+ accountNumber);
         System.out.print("Enter initial balance : ");
         balance = sc.nextInt();
+        System.out.println("Your Initial balance is : "+balance);
     }
     void login()
     {
@@ -42,7 +48,21 @@ class BankAccount
         upin = sc.next();
         if(uid.equals(id) && upin.equals(pin))
         {
-            System.out.println("Login Successful");
+            ATM atm = new ATM();
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            int choice = sc.nextInt();
+            switch(choice)
+            {
+                case 1:
+                    atm.deposit();
+                    break;
+                case 2:
+                    atm.withdraw();
+                    break;
+                default:
+                    System.out.println("Enter valid choice");
+            }
         }
         else
         {
